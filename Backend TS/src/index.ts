2f4e67436a68f706ec0aba79ffb69a49b2113446
apurbopal248 <apurbopal248@gmail.com>
2025-08-12 02:08:00 +1000

    Succesfully connected with MongoDB

--- Backend TS/src/index.ts
+++ Backend TS/src/index.ts
@@ -1,12 +1,33 @@
-import express from "express";
+// Initialize express server
 
+import express ,{ Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectedToMongoDB } from "./utils/connectToMongoDB ";
+
+
+dotenv.config();
+
+
+const port : number|string = process.env.PORT || 7000;
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello from Hospital Management System backend!");
+
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.get("/", (req:Request, res:Response): void => {
+  res.send("wellcome to Hospital Management System Server :)");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+connectedToMongoDB()
+.then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})
+.catch((error) => {
+  console.error("Failed to connect to MongoDB:", error);
+  process.exit(1);
 });
---
+++ Backend TS/src/utils/connectToMongoDB .ts
@@ -0,0 +1,24 @@
+// connect to MongoDB utility
+// This file is responsible for connecting to MongoDB using Mongoose
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI
+
+
+export const connectedToMongoDB = async (): Promise<void> => {
+  try {
+    if (!MONGODB_URI) {
+  console.error("❌ MONGODB_URI is not defined in environment variables");
+  process.exit(1);
+}
+    await mongoose.connect(MONGODB_URI );
+
+    console.log("✅ Connected to MongoDB");
+  } catch (error: any) {
+    console.error(`❌ Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
